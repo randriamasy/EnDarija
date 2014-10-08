a85d1e064b9dd28ee69872fe988b10419be5815c
Stéphano Telolahy <stephano.telolahy@gmail.com>
2014-10-08 13:08:58 +0100

    search events

--- app/src/main/java/com/endarija/DictionaryActivity.java
+++ app/src/main/java/com/endarija/DictionaryActivity.java
@@ -8,54 +8,65 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class DictionaryActivity extends ActionBarActivity {
+
+    private DictionaryModel model;
+    ListView listView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
-        ListView listView1 = (ListView) findViewById(R.id.listView1);
+        model = new DictionaryModel(this);
+        listView1 = (ListView) findViewById(R.id.listView1);
 
-        DictionaryModel model = new DictionaryModel(this);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, model.allWords());
-
-        listView1.setAdapter(adapter);
+        reloadListViewWithAllWords();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_action_bar, menu);
         MenuItem searchItem = menu.findItem(R.id.action_search);
         SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                // TODO: remove keyboard
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                // update listView content
+                reloadListViewWithWordsContainingKey(s);
+                return false;
+            }
+        });
+
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_search) {
 
-            ListView listView1 = (ListView) findViewById(R.id.listView1);
+    private void reloadListViewWithAllWords() {
 
-            DictionaryModel model = new DictionaryModel(this);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, model.allWords());
+        listView1.setAdapter(adapter);
 
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                    android.R.layout.simple_list_item_1, model.wordsWithKey("bon"));
-
-            listView1.setAdapter(adapter);
-
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
+
+    private void reloadListViewWithWordsContainingKey(String key) {
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, model.wordsWithKey(key));
+        listView1.setAdapter(adapter);
+    }
+
 }
